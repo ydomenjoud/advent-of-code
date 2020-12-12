3ccf14c66c9fb29eee23a1cf4eafa1fdd3fc10cf
ydomenjoud <yannick@6tech.net>
2020-12-13 00:47:11 +0100

    Initial commit

---
+++ tools.ts
@@ -0,0 +1,21 @@
+import { readLines } from 'https://deno.land/std@0.80.0/io/bufio.ts';
+
+export async function *readline(file: string) {
+    const f = await Deno.open(file);
+    for await (const line of readLines(f)) {
+        if (line) {
+            yield line;
+        }
+    }
+    f.close();
+}
+
+export async function lineToArray(file: string): Promise<string[]>{
+    const input: string[] = [];
+    for await (const line of readline(file)) {
+        if (line && line.trim() !== '') {
+            input.push(line);
+        }
+    }
+    return input;
+}
